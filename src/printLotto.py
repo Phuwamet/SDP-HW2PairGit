25172b9edd77c075a41a67adebb3e273bf36135a
Phuwamet Rattantiwatthana <ra.phuwamet_st@tni.ac.th>
2016-12-16 03:45:08 +0000

    First commit

--- src/printLotto.py
+++ src/printLotto.py
@@ -1,10 +1,11 @@
 from random import randint
-a="
-for x in range(0, 5):
-    b=randint(0,9);
-    if x==0 and b==0 :
-        a+=str(0);
-    else:
-        a+=str(b);
-
-print a[:];
+a=""
+for x in range(1, 5):
+    for i in range(0,3):
+        b=randint(0,9);
+        if x==0 and b==0 :
+            a+=str(0);
+        else:
+            a+=str(b);
+    print "Lotto",x,a[:];
+    a=""
